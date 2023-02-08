854cf099f2a28c9638ec778edd92b6a18c54d41c
Re-Animator <reanimator1904@gmail.com>
2023-02-08 17:58:06 +0400

    v.0.1

--- app/src/main/java/com/reanimator/moviesapp/MainActivity.kt
+++ app/src/main/java/com/reanimator/moviesapp/MainActivity.kt
@@ -10,34 +10,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.reanimator.moviesapp.navigation.SetupNavHost
 import com.reanimator.moviesapp.ui.theme.MoviesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val navController = rememberNavController()
+            SetupNavHost(navController = navController)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoviesAppTheme {
-        Greeting("Android")
     }
 }
